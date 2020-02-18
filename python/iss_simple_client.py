9404974173456a93ef3607a638c94f949fb21ad4
zaqwes <igor.a.lugansky@gmail.com>
2020-02-18 22:39:42 +0300

    get candles for FXRU

--- python/iss_simple_client.py
+++ python/iss_simple_client.py
@@ -13,8 +13,10 @@ import base64
 import cookielib
 import json
 
-
-requests = {'history_secs': 'http://iss.moex.com/iss/history/engines/%(engine)s/markets/%(market)s/boards/%(board)s/securities.json?date=%(date)s'}
+requests = {
+    'history_secs': 'http://iss.moex.com/iss/history/engines/%(engine)s/markets/%(market)s/boards/%(board)s/securities.json?date=%(date)s',
+    'candles_day': 'http://iss.moex.com/iss/history/engines/%(engine)s/markets/%(market)s/boards/%(board)s/securities/%(ticker)s/candles.json?from=%(start_date)s&till=%(stop_date)s&interval=24'
+}
 
 
 class Config:
@@ -25,7 +27,7 @@ class Config:
             proxy_url: proxy URL if any is used, specified as http://proxy:port
             debug_level: 0 - no output, 1 - send debug info to stdout
         """
-        self.debug_level = debug_level  
+        self.debug_level = debug_level
         self.proxy_url = proxy_url
         self.user = user
         self.password = password
@@ -79,6 +81,7 @@ class MicexISSDataHandler:
     """ Data handler which will be called
     by the ISS client to handle downloaded data.
     """
+
     def __init__(self, container):
         """ The handler will have a container to store received data.
         """
@@ -152,6 +155,7 @@ class MicexISSClient:
                 result.append((sec[secIdx],
                                del_null(sec[closeIdx]),
                                del_null(sec[tradesIdx])))
+
             # we return pieces of received data on each iteration
             # in order to be able to handle large volumes of data
             # and to start data processing without waiting for
@@ -159,6 +163,62 @@ class MicexISSClient:
             self.handler.do(result)
             cnt = len(jdata)
             start = start + cnt
+        return True
+
+    def get_history_securities_by_range(self, engine, market, board, ticker, start_date, stop_date):
+        """ Get and parse historical data on all the securities at the
+        given engine, market, board
+        """
+        url = requests['candles_day'] % {'engine': engine,
+                                         'market': market,
+                                         'board': board,
+                                         'ticker': ticker,
+                                         'start_date': start_date,
+                                         'stop_date': stop_date
+                                         }
+
+        # always remember about the 'start' argument to get long replies
+        start = 0
+        cnt = 1
+        print(url)
+        while cnt > 0:
+            res = self.opener.open(url + '&start=' + str(start))
+            jres = json.load(res)
+
+            # the following is also just a simple example
+            # it is recommended to keep metadata separately
+
+            # root node with historical data
+            jhist = jres['history']
+
+            # node with actual data
+            jdata = jhist['data']
+
+            # node with the list of column IDs in 'data' in correct order;
+            # it's also possible to use the iss.json=extended argument instead
+            # to get all the IDs together with data (leads to more traffic)
+
+            jcols = jhist['columns']
+            dateIdx = jcols.index('TRADEDATE')
+            secIdx = jcols.index('SECID')
+            closeIdx = jcols.index('LEGALCLOSEPRICE')
+            tradesIdx = jcols.index('NUMTRADES')
+
+            result = []
+            for sec in jdata:
+                result.append((sec[dateIdx], sec[secIdx],
+                               del_null(sec[closeIdx]),
+                               del_null(sec[tradesIdx])))
+            # we return pieces of received data on each iteration
+            # in order to be able to handle large volumes of data
+            # and to start data processing without waiting for
+            # the complete reply
+
+            self.handler.do(result)
+
+            cnt = len(jdata)
+            start = start + cnt
+
         return True
 
 
--- python/iss_simple_main.py
+++ python/iss_simple_main.py
@@ -14,6 +14,8 @@
 
 import sys
 
+from os.path import expanduser
+
 from iss_simple_client import Config
 from iss_simple_client import MicexAuth
 from iss_simple_client import MicexISSClient
@@ -25,21 +27,23 @@ class MyData:
     Kept separately from the handler for scalability purposes: in order
     to differentiate storage and output from the processing.
     """
+
     def __init__(self):
         self.history = []
 
     def print_history(self):
         print "=" * 49
-        print "|%15s|%15s|%15s|" % ("SECID", "CLOSE", "TRADES")
+        print "|%15s|%15s|%15s|%15s|" % ("TRADEDATE", "SECID", "CLOSE", "TRADES")
         print "=" * 49
         for sec in self.history:
-            print "|%15s|%15.2f|%15d|" % (sec[0], sec[1], sec[2])
+            print "|%15s|%15s|%15.2f|%15d|" % (sec[0], sec[1], sec[2], sec[3])
         print "=" * 49
 
 
 class MyDataHandler(MicexISSDataHandler):
     """ This handler will be receiving pieces of data from the ISS client.
     """
+
     def do(self, market_data):
         """ Just as an example we add all the chunks to one list.
         In real application other options should be considered because some
@@ -49,17 +53,35 @@ class MyDataHandler(MicexISSDataHandler):
 
 
 def main():
-    user=''
-    password = ''
+    f = open(expanduser("~") + "/credentials", "r")
+    pair = f.read()
+
+    user, password = pair.split(' ')
+    user = user.strip()
+    password = password.strip()
     my_config = Config(user=user, password=password, proxy_url='')
     my_auth = MicexAuth(my_config)
     if my_auth.is_real_time():
         iss = MicexISSClient(my_config, my_auth, MyDataHandler, MyData)
-        iss.get_history_securities('stock',
-                                   'shares',
-                                   'eqne',
-                                   '2020-01-12')
+        # iss.get_history_securities(engine='stock',
+        #                            market='shares',
+        #                            board='tqtf',
+        #                            date='2020-02-12')
+        # iss.handler.data.print_history()
+
+        # from=2010-08-23&till=2010-08-24
+        iss.get_history_securities_by_range(engine='stock',
+                                            market='shares',
+                                            board='tqtf',
+                                            ticker='FXRU',
+                                            start_date='2019-01-12',
+                                            stop_date='2020-02-12',
+                                            )
         iss.handler.data.print_history()
+
+    # http://iss.moex.com/iss/history/engines/stock/markets/shares/boards/tqtf/securities.json?from=2018-08-08&till=2018-08-25&interval=24&start=0
+    # http://iss.moex.com/iss/history/engines/stock/markets/shares/boards/tqtf/securities/FXRU/candles.json?from=2018-08-08&till=2018-08-25&interval=24&start=0
+
 
 if __name__ == '__main__':
     try:
